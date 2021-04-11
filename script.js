052f7103bb91cbc492477bc9d811ec18d3e2f091
jameslagbeneku <64613959+jameslagbeneku@users.noreply.github.com>
2021-04-10 22:23:26 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,21 @@ const tempIcon = document.querySelector(".weather-icon")
 const weatherType = document.querySelector(".weather-type")
 const weatherDesc = document.querySelector(".desc")
 const container = document.querySelector(".container")
+const forecast = document.querySelector(".forecast")
+const next48Box = document.querySelector(".next-48-box")
+const nextHour = document.querySelector(".hour")
+const nextTimeOfDay = document.querySelector(".time-of-day")
+const nextTemp = document.querySelector(".next-48hr-temp")
+const nextType = document.querySelector(".next-48hr-weather-type")
+const nextUnit = document.querySelector(".next-48hr-unit")
+const nextIcon = document.querySelector(".next-48hr-weather-icon")
+const sevenDayForecast = document.querySelector(".seven-day-forecast")
+const nextSevenBox = document.querySelector(".next-7-box")
+const nextDay = document.querySelector(".day")
+const nextMinTemp = document.querySelector(".min-temp")
+const nextMaxTemp = document.querySelector(".max-temp")
+const nextDayIcon = document.querySelector(".next-seven-icon")
+const footerYear = document.querySelector(".year")
 const weatherAPIKey = '8dc3d31ab463cd964459f51f1846f13a'
 const locationAPIKey = '52e2cd6a10ad403751aa7d2f0a196a6280a1e76a70047dd6b829a1e8'
 var weatherCity;
@@ -14,65 +29,164 @@ var temp;
 var tempType;
 var tempDesc;
 var icon;
-
+var latitude;
+var longitude;
 
 var d = new Date()
 var h = d.getHours()
-
-
-
-let backgroundChange = function () {
-  if (h > 6, h < 18) {
-    container.style.background = "url('img/daybackground.gif') #006fad no-repeat cover center";
+var y = d.getFullYear()
+footerYear.innerHTML = y
+ 
+  if (h < 18) {
+    container.style.background = `url('img/daybackground.gif') #006fad`;
+    container.style.backgroundSize = `cover`;
+    container.style.backgroundRepeat = `no-repeat`
+    container.style.backgroundPosition = `center`
   } else {
-    container.style.background = "url('img/nightbackground.gif') #003959 no-repeat cover center";
+    container.style.background = `url('img/nightbackground.gif') #003959`;
+    container.style.backgroundSize = `cover`;
+    container.style.backgroundRepeat = `no-repeat`
+    container.style.backgroundPosition = `center`
   }  
-}
 
-
-
-
-let weather = function () {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${weatherCity},${countryCode}&appid=${weatherAPIKey}`)
-  .then(response => response.json())
-  .then(weatherData => {
-    console.log(weatherData)
-    temp = (weatherData.main.temp - 273.15) * 9/5 + 32;
-    tempType = weatherData.weather[0].main;
-    tempDesc = weatherData.weather[0].description;
-    icon = weatherData.weather[0].icon;
-    tempFigure.innerHTML = Math.ceil(temp);
-    weatherType.innerHTML = tempType;
-    weatherDesc.innerHTML = tempDesc;
-    tempIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-    
-    if (countryCode !== "US") {
-      temp = weatherData.main.temp - 273.15;
-      tempFigure.innerHTML = Math.ceil(temp)
-      unit.innerHTML = "°C"
-      console.log(temp)
-    } else {
-      temp = (weatherData.main.temp - 273.15) * 9/5 + 32;
-      unit.innerHTML = "°F";
+  const successCallback = (position) => {
+    var latitude = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    let newWeather = function () {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${weatherAPIKey}`)
+      .then(response => response.json())
+      .then(newWeatherData => {
+        temp = (newWeatherData.main.temp - 273.15) * 9/5 + 32;
+        tempType = newWeatherData.weather[0].main;
+        tempDesc = newWeatherData.weather[0].description;
+        icon = newWeatherData.weather[0].icon;
+        weatherCity = newWeatherData.name
+        countryCode = newWeatherData.sys.country;
+        city.innerHTML = weatherCity
+        country.innerHTML = countryCode
+        tempFigure.innerHTML = Math.ceil(temp);
+        weatherType.innerHTML = tempType;
+        weatherDesc.innerHTML = tempDesc;
+        tempIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+      
+      });
     }
-  });
-}
 
-fetch('https://api.ipdata.co/?api-key=52e2cd6a10ad403751aa7d2f0a196a6280a1e76a70047dd6b829a1e8')
-  .then(response => response.json())
-  .then(data => {
-    weatherCity = data.city;
-    countryCode = data.country_code;
-    city.innerHTML = weatherCity
-    country.innerHTML = countryCode
+    let twentyFourHourWeather = function () {
+      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,alerts&appid=${weatherAPIKey}`)
+      .then(response => response.json())
+      .then(twentyFourHourWeather => {
+        
+        const weatherArray = Object.entries(twentyFourHourWeather.hourly)
+        weatherArray.splice(25, 24)
+        weatherArray.shift()
+  
+        nextTemp.innerHTML = Math.ceil(((weatherArray[0][1].temp) - 273.15) * 9/5 + 32);
+        nextType.innerHTML = weatherArray[0][1].weather[0].main;
+        newIcon = weatherArray[0][1].weather[0].icon;
+        nextIcon.src = `http://openweathermap.org/img/wn/${newIcon}.png`;
+  
+        weatherArray.forEach(output);
+  
+        function output(item, index){
+  
+          const milliseconds = weatherArray[index][1].dt;
+          var myDate = new Date(milliseconds*1000);
+          var nextHours = myDate.getHours()
+          nextHour.innerHTML = nextHours
+  
+          if (nextHours > 12) {
+              nextHours = myDate.getHours() - 12;
+              nextTimeOfDay.innerHTML = " PM"
+          } else if (nextHours === 0) {
+            nextHours = "12"
+            nextTimeOfDay.innerHTML = " AM" 
+          } else if (nextHours === 12) {
+            nextTimeOfDay.innerHTML = " PM"
+          }
+          else {
+            nextHours = myDate.getHours();
+            nextTimeOfDay.innerHTML = " AM"
+          }
+          
+          nextTemp.innerHTML = Math.ceil(((weatherArray[index][1].temp) - 273.15) * 9/5 + 32);
+          nextType.innerHTML = weatherArray[index][1].weather[0].main;
+          newIcon = weatherArray[index][1].weather[0].icon;
+          nextIcon.src = `http://openweathermap.org/img/wn/${newIcon}.png`;
+          const newForecast = document.createElement("div");
+          forecast.appendChild(newForecast);
+          newForecast.classList.add("next-48-box")
+          newForecast.innerHTML = `
+          <div class="time">
+          <p class="hour">${nextHours}</p> <p class="time-of-day">${nextTimeOfDay.innerHTML}</p>
+          </div>
+          <div class="next-48">
+              <p class="next-48hr-temp">${nextTemp.innerHTML}</p>
+              <p class="next-48hr-unit">°F</p>
+          </div>
+          <img class="next-48hr-weather-icon" src="${nextIcon.src}" alt="">
+          <p class="next-48hr-weather-type">${nextType.innerHTML}</p>
+          `
+          if (countryCode !== "US") {
+                    nextTemp.innerHTML = Math.ceil(((weatherArray[index][1].temp) - 273.15));
+                    unit.innerHTML = "°C"
+                  }
+        }
+      
+      });
+    }
 
-    backgroundChange()
-    weather()
-    
-  });
+    let sevenDayWeather = function () {
+      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,alerts&appid=${weatherAPIKey}`)
+      .then(response => response.json())
+      .then(sevenDayWeather => {
+        
+        const sevenDayArray = Object.entries(sevenDayWeather.daily)
+        sevenDayArray.shift()
+        nextDayIcon.src = `http://openweathermap.org/img/wn/${newIcon}.png`;
   
+        sevenDayArray.forEach(output);
   
-  // console.log(math.round(55.44746))
+        function output(item, index){
+  
+          const milliseconds = sevenDayArray[index][1].dt;
+          var myDate = new Date(milliseconds*1000);
+          var dateSplit = myDate.toLocaleDateString(undefined, {weekday: 'long'}).split(" ")
+          nextDay.innerHTML = dateSplit[0]
+          nextMinTemp.innerHTML = Math.ceil(((sevenDayArray[index][1].temp.min) - 273.15) * 9/5 + 32);
+          nextMaxTemp.innerHTML = Math.ceil(((sevenDayArray[index][1].temp.max) - 273.15) * 9/5 + 32);
+          newIcon = sevenDayArray[index][1].weather[0].icon;
+          nextDayIcon.src = `http://openweathermap.org/img/wn/${newIcon}.png`;;
+          const newDay = document.createElement("div");
+          sevenDayForecast.appendChild(newDay);
+          newDay.classList.add("next-7-box")
+          newDay.innerHTML = `
+          <p class="day">${nextDay.innerHTML}</p>
+          <p class="min-temp">${nextMinTemp.innerHTML} °</p>
+          <p class="max-temp">${nextMaxTemp.innerHTML} °</p>
+          <img class="next-seven-icon" src="${nextDayIcon.src}" alt="">
+          `
+          if (countryCode !== "US") {
+                    nextMinTemp.innerHTML = Math.ceil(((weatherArray[index][1].temp) - 273.15));
+                    nextMaxTemp.innerHTML = Math.ceil(((weatherArray[index][1].temp) - 273.15));
+                  }
+        }
+      
+      });
+    }
+  
+    newWeather()
+    twentyFourHourWeather()
+    sevenDayWeather()
+  };
+  
+  const errorCallback = (error) => {
+    console.error(error);
+  };
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+
   
 
+  
   
